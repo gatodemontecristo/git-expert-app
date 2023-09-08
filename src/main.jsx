8657310d01a387e04d5df5f493bd222c9ecd3f29
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-09-08 00:00:19 -0500

    Agregando estilos

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GifExpertApp } from './GifExpertApp'
 
+import './styles.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* tituklo */}
     <GifExpertApp />
+    {/* input */}
+    {/* Listado de Gif */}
+    {/* Gif Item */}
   </React.StrictMode>,
 )
