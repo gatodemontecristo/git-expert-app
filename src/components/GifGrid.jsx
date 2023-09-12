c8515c392a217536040f3b4958e2398e13cfe831
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-09-11 23:59:36 -0500

    API

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,22 @@
 
 
 export const GifGrid = ({category}) => {
+
+  const getGifs = async() =>{
+    const url = `api.giphy.com/v1/gifs/search?api_key=Dfc53ziq3QwMa9YyR4iG66WyB4Ey647v&q=${category}&limit=20`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map(img=>({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url
+    }))
+    console.log(gifs);
+  }
+
+  getGifs();
+
   return (
     <>
         <h3>{category}</h3>
